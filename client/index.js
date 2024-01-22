117320a57f5ad7ced01de99c9c3303ee1005175e
ivshxvi <ivhsxvi@gmail.com>
2024-01-22 13:55:53 +0000

    Work on index.js still todo

--- client/index.js
+++ client/index.js
@@ -1,8 +1,7 @@
 const key = '41828339-4484571af3c8494d167e1d483'
 
 const displayForm = document.querySelector("#displaySection")
-const startButton = document.querySelector("startGame button")
-
+const startButton = document.querySelector("#gameplaySection button")
 
 
 const displayCapitalName = () => {
@@ -13,14 +12,38 @@ const displayCapitalName = () => {
     fetch("http://localhost:3000/random")
     .then((data) => data.json())
     .then((item) => {
+        console.log(typeof item.name)
+        console.log(typeof userInput)
+        console.log(userInput.value)
         capitalName.textContent = item.capital
-        if(userInput.toLowerCase() == item.name.toLowerCase()) {
-            message.textContent = `You guessed right, this is ${item.name}`
-        } else {
-            message.textContent = "You are wrong!"
-        }
+        getPicture(item.capital)
+
+        // THE LOGIC BELOW SHOULD BE IN AN ASYNC FUNCTION
+
+        // if(userInput.value.toLowerCase() === item.name.toLowerCase()) {
+        //     message.textContent = `You guessed right, this is ${item.name}`
+        // } else {
+        //     message.textContent = "You are wrong!"
+        // }
     })
 }
 
 startButton.addEventListener('click', displayCapitalName)
 
+const getPicture = async (capital) => {
+    try{
+        const resp = await fetch(`https://pixabay.com/api/?key=${key}&q=${capital}`)
+        const data = await resp.json()
+        placePicture(data.hits[0].previewURL)
+    } catch(err){
+        console.error(err)
+    }   
+}
+
+const placePicture = (picUrl) => {
+    const pic = document.createElement("img")
+    pic.src = picUrl
+    pic.addEventListener("click", (f) => f.target.remove(),{once:true})
+    gameplaySection.appendChild(pic)
+    
+}
