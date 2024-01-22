dd3ec326566a354286b964315376bac25e1db15e
ivshxvi <ivhsxvi@gmail.com>
2024-01-22 09:33:00 +0000

    Work in progress on app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+const geo = require('./countries+states+cities.json')
+const logger = require('./logger.js')
+
+app.use(cors())
+app.use(express.json())
+app.use(logger)
+
+app.get("/geo/:country", (req,res) => {
+    const country = req.params.country.toLowerCase()
+    const geoFiltered = geo.filter((f) => f.name.toLowerCase() == country)
+
+    geoFiltered.length === 0
+    ? res.status(404).send("Such country does not exist")
+    : res.send(geoFiltered)
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
