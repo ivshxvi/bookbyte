6a8ea0ace65e59946acfa9944999f3c247d35dda
Usama Navid <usamanavid098@gmail.com>
2024-01-24 10:29:34 +0000

    Add timer

--- client/index.js
+++ client/index.js
@@ -1,138 +1,131 @@
-
-
-const displayForm = document.querySelector("#displaySection")
-const startButton = document.querySelector("#gameplaySection button")
-const submitButton = document.querySelector("#userInputSection")
-const scoreBoard = document.querySelector("#score")
-
+const displayForm = document.querySelector("#displaySection");
+const startButton = document.querySelector("#gameplaySection button");
+const submitButton = document.querySelector("#userInputSection");
+const scoreBoard = document.querySelector("#score");
+const timerElement = document.querySelector("#timer");
+const timesUpSection = document.querySelector("#timesUp");
+const timesUpMessage = timesUpSection.querySelector("#timesUpMessage");
+const finalScoreElement = timesUpSection.querySelector("#finalScore");
+const scoreDisplayElement = timesUpSection.querySelector("#scoreDisplay");
 
 const myFunction = () => {
-    document.getElementById("myDropdown").classList.toggle("show")
-}
+    document.getElementById("myDropdown").classList.toggle("show");
+};
 
 window.onclick = (e) => {
     if (!e.target.matches('.dropbtn')) {
-        const myDropdown = document.getElementById("myDropdown")
+        const myDropdown = document.getElementById("myDropdown");
         if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show')
+            myDropdown.classList.remove('show');
         }
     }
-}
-
+};
 
 window.addEventListener('beforeunload', (e) => {
-    const confirmationMessage = 'Are you sure you want to leave? Your score will be lost!'
+    const confirmationMessage = 'Are you sure you want to leave? Your score will be lost!';
+    e.returnValue = confirmationMessage || undefined;
+    return confirmationMessage;
+});
 
-    e.returnValue = confirmationMessage || undefined
-    return confirmationMessage
-})
+document.querySelector("#userInputSection").style.display = "none";
 
-document.querySelector("#userInputSection").style.display = "none"
-
-
-
-let countryData
-
-
-
+let countryData;
+let score = 0;
+let successHistory = [];
 
 const getCountry = async () => {
     try {
-        let data
+        let data;
         do {
-            const response = await fetch('http://localhost:3000/random')
-            data = await response.json()
-        } while (successHistory.some(entry => entry.name === data.name))
+            const response = await fetch('http://localhost:3000/random');
+            data = await response.json();
+        } while (successHistory.some(entry => entry.name === data.name));
 
-        countryData = data
+        countryData = data;
 
-        return data
-    } catch (e) { console.error(e) }
-}
+        return data;
+    } catch (e) {
+        console.error(e);
+    }
+};
 
+// Function to update and display the timer
+const updateTimer = () => {
+    timerElement.textContent = `Time Left: ${timer}`;
+    if (timer === 0) {
+        timesUpSection.style.display = "block"; // Show the entire section
+        finalScoreElement.style.display = "block"; // Show the final score element
+        scoreDisplayElement.textContent = score; // Update the final score display
+        submitButton.removeEventListener("submit", onSubmit);
+        console.log("Times Up! Final Score: " + score);
+    } else {
+        timer--;
+        setTimeout(updateTimer, 1000); // Update timer every 1 second
+    }
+};
 
-
-
-
-startButton.addEventListener('click', (e) => {
-    const capitalName = document.querySelector("#capitalName")
-    const randomiseTag = document.querySelector("#randomise")
-    e.preventDefault()
-    document.querySelector("#userInputSection").style.display = "block"
-    getCountry().then((data) => {
-        removePicture()
-        console.log(data.name)
-        capitalName.textContent = data.capital
-        placePicture(data.capital_picture)
-        message.textContent = ""
-        randomiseTag.textContent = "Next"
-    })
-
-})
-
-let score = 0
-let successHistory = []
-
-submitButton.addEventListener("submit", async (e) => {
-    e.preventDefault()
+const onSubmit = async (e) => {
+    e.preventDefault();
     const userInput = document.getElementById('userInput').value;
-    const message = document.querySelector("#message")
+    const message = document.querySelector("#message");
 
     if (countryData.name.includes(userInput)) {
-        score += 1
-        scoreBoard.textContent = `Score: ${score}`
+        score += 1;
+        scoreBoard.textContent = `Score: ${score}`;
         getCountry().then((data) => {
-            removePicture()
-            console.log(data.name)
-            capitalName.textContent = data.capital
-            placePicture(data.flag)
-            message.textContent = ""
-        })
-        e.target.userInput.value = ""
-    }
-    else if (countryData.name.includes(userInput) && userInput.length >= 4) {
-        message.textContent = "Nearly there!"
-        message.classList.add('flash-orange')
-    }
-    else {
-//
-
-//     if (countryData.name.includes(userInput)){
-//         message.textContent = `You guessed right, this is ${countryData.name}`
-//         message.classList.add('flash-green')
-//         score += 1
-//         scoreBoard.textContent = `Score: ${score}`
-//         successHistory.push(countryData)
-//         console.log(successHistory)
-
-//     } else {
-// 
-        message.textContent = "You are wrong!"
-
-        message.classList.add('flash-red')
-
-        e.target.userInput.value = ""
-
+            removePicture();
+            console.log(data.name);
+            capitalName.textContent = data.capital;
+            placePicture(data.flag);
+            message.textContent = "";
+        });
+        e.target.userInput.value = "";
+    } else if (countryData.name.includes(userInput) && userInput.length >= 4) {
+        message.textContent = "Nearly there!";
+        message.classList.add('flash-orange');
+    } else {
+        message.textContent = "You are wrong!";
+        message.classList.add('flash-red');
+        e.target.userInput.value = "";
     }
     setTimeout(() => {
-        message.classList.remove('flash-red', 'flash-orange')
-    }, 500)
-})
-
+        message.classList.remove('flash-red', 'flash-orange');
+    }, 500);
+};
 
 const placePicture = (picUrl) => {
-    const pic = document.createElement("img")
-    pic.src = picUrl
-    pic.classList.add("img")
-    pic.addEventListener("click", (f) => f.target.remove(), { once: true })
-    gameplaySection.appendChild(pic)
-
-}
+    const pic = document.createElement("img");
+    pic.src = picUrl;
+    pic.classList.add("img");
+    pic.addEventListener("click", (f) => f.target.remove(), { once: true });
+    gameplaySection.appendChild(pic);
+};
 
 const removePicture = () => {
-    const pic = document.querySelector(".img")
+    const pic = document.querySelector(".img");
     if (pic) {
-        pic.remove()
+        pic.remove();
     }
-}
+};
 
+startButton.addEventListener('click', (e) => {
+    const capitalName = document.querySelector("#capitalName");
+    const randomiseTag = document.querySelector("#randomise");
+    e.preventDefault();
+    document.querySelector("#userInputSection").style.display = "block";
+    getCountry().then((data) => {
+        removePicture();
+        console.log(data.name);
+        capitalName.textContent = data.capital;
+        placePicture(data.capital_picture);
+        message.textContent = "";
+        randomiseTag.textContent = "Next";
+    });
+
+    // Reset the timer and start the countdown
+    timer = 10;
+    updateTimer();
+
+    // Attach the submit event listener
+    submitButton.addEventListener("submit", onSubmit);
+});
